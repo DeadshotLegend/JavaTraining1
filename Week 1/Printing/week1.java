ba8c98ea9805906cf919107696da66574efe43fd
DeadshotLegend <shivanshggamer@gmail.com>
2022-09-27 17:42:31 -0700

    Comitting week1, week 2 hacks

---
+++ Week 1/Printing/week1.java
@@ -0,0 +1,9 @@
+
+public class week1 {
+
+    public static void main(String[] args) {
+        System.out.println("Java is cool!");
+        System.out.println("Your mom is cooler");
+    }
+    
+}
---
+++ Week 2/Arithmetics/code.java
@@ -0,0 +1,9 @@
+public class code {
+
+public static void main(String[] args) {
+int num = ((6 * 1234124/11345 - 890809 + 90800)%980098);
+System.out.println(num/100);
+
+}
+
+}
